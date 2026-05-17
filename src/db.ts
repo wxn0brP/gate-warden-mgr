ee32551fd1ed26c55fee45910d93c303fa2d6466
wxn0brP <toja.filiper@gmail.com>
2026-05-17 19:42:04 +0200

    chore: bump deps

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
-import { Valthera } from "@wxn0brp/db";
+import { ValtheraCreate } from "@wxn0brp/db";
 import { UserManager, WardenManager } from "@wxn0brp/gate-warden";
 import { config } from "./config";
 
-export const db = new Valthera(config.dir);
+export const db = ValtheraCreate(config.dir);
 export const wardenMgr = new WardenManager(db);
 export const userMgr = new UserManager(db);
